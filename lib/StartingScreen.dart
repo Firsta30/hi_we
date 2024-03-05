c5f6f525b03709922aa6989ca000d181ab7aa5b6
Firsta30 <91885471+Firsta30@users.noreply.github.com>
2024-03-06 01:02:10 +0700

    new package and modify Starting Screen dart

--- lib/StartingScreen.dart
+++ lib/StartingScreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hi_we/NextScreen.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class StartingScreen extends StatefulWidget {
   @override
@@ -13,10 +16,37 @@ class _StartingScreenState extends State<StartingScreen> {
         height: 50,
         child: BottomAppBar(
           shape: CircularNotchedRectangle(),
-          color: const Color.fromARGB(255, 255, 48, 117),
+          color: Color.fromARGB(255, 254, 60, 124),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NextScreen(),
+              ));
+        },
+        child: Icon(
+          FlutterIcons.play_circle_outline_mdi,
+          color: Color.fromARGB(255, 232, 91, 91),
+          size: 55,
+        ),
+        backgroundColor: Color.fromARGB(255, 250, 223, 223),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: WaveClipperTwo(),
+            child: Container(
+              height: 220,
+              decoration: BoxDecoration(
+                  image: DecorationImage(image: AssetImage("image/"))),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
